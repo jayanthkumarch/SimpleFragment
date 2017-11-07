5e00c3dfa72b04454824e834962f81cbd26b960a
jayanthkumarch <jayanthkumarch@gmail.com>
2017-11-07 10:30:56 +0530

    removed comment and unnecassary space

--- token_gen.py
+++ token_gen.py
@@ -6,11 +6,6 @@ import json
 import httplib
 import ssl
 import datetime
-#Added comment
-#Added hot fix commment more coment 
-#Added two  more coment from jayanth branch  
-
-
 
 conn = httplib.HTTPSConnection("www-test-ngp-cp.csp.infoblox.com",context=ssl._create_unverified_context())
 request_header = {'Content-Type':'application/json','Accept': 'application/json'}
